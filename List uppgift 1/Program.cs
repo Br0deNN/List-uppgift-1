bee5539145084a483bd6042f1893a243a9b043b0
7R1XY <theo.broden@gmail.com>
2024-02-02 12:29:59 +0100

    Add project files.

---
+++ List uppgift 1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic; 
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<string> lista = new List<string>();
+
+        Console.WriteLine("Nu ska du skriva in namn");
+        string namn = "empty";
+
+
+        while (namn != "")
+        {
+            Console.WriteLine("Skriv in ett namn");
+            namn = Console.ReadLine();
+            if (namn != "")
+            {
+                lista.Add(namn);
+            }
+        }
+
+        Console.WriteLine("Här är de namn du skrev fast bakänges");
+        lista.Reverse();
+
+        foreach (string s in lista)
+        {
+            Console.WriteLine(s);
+        }
+
+        Console.ReadKey();
+    }
+}
